e70d4d8a9174eb8a93a28d89916b6b0dba5e9220
Sal Rahman <salehen.rahman@gmail.com>
2024-03-22 22:31:51 -0700

    API is shaping up really nicely

--- parse.go
+++ parse.go
@@ -8,13 +8,9 @@ import (
 	"github.com/piprate/json-gold/ld"
 )
 
-type Parsed struct {
-	proc    *ld.JsonLdProcessor
-	options *ld.JsonLdOptions
-	payload []any
-}
+type LDNodes []any
 
-func Parse(b []byte, options *ld.JsonLdOptions) (*Parsed, error) {
+func Parse(b []byte, options *ld.JsonLdOptions) (LDNodes, error) {
 	if options == nil {
 		options = ld.NewJsonLdOptions("")
 	}
@@ -33,19 +29,19 @@ func Parse(b []byte, options *ld.JsonLdOptions) (*Parsed, error) {
 		return nil, err
 	}
 
-	return &Parsed{proc: proc, payload: i}, nil
+	return LDNodes(i), nil
 }
 
-func (p *Parsed) Unmarshal(dest any) error {
+func (p LDNodes) UnmarshalTo(dest any) error {
 	value := reflect.ValueOf(dest)
 	if value.Kind() == reflect.Ptr {
 		value = value.Elem()
 	}
 	if value.Kind() != reflect.Slice {
-		if len(p.payload) != 1 {
+		if len(p) != 1 {
 			return errors.New("expected a maximum of 1 node in the list of parsed nodes")
 		}
-		b, err := json.Marshal(p.payload[0])
+		b, err := json.Marshal(p[0])
 		if err != nil {
 			return err
 		}
@@ -56,10 +52,38 @@ func (p *Parsed) Unmarshal(dest any) error {
 		return nil
 	}
 
-	b, err := json.Marshal(p.payload)
+	b, err := json.Marshal(p)
 	if err != nil {
 		return err
 	}
 
 	return json.Unmarshal(b, &dest)
+}
+
+func (p LDNodes) Items() <-chan *LDNodes {
+	c := make(chan *LDNodes)
+	go func() {
+		defer close(c)
+		for _, item := range p {
+			c <- &LDNodes{item}
+		}
+	}()
+	return c
+}
+
+// UnmarshalJSON is used unmarshal an expanded document into something that is
+// easier to work with.
+//
+// Should NOT be used with an unexpanded JSON object!
+func (p *LDNodes) UnmarshalJSON(b []byte) error {
+	var a []any
+
+	err := json.Unmarshal(b, &a)
+	if err != nil {
+		return err
+	}
+
+	*p = LDNodes(a)
+
+	return nil
 }
--- parse_test.go
+++ parse_test.go
@@ -1,6 +1,16 @@
-package prettyld
+package prettyld_test
 
-import "testing"
+import (
+	"slices"
+	"testing"
+
+	"github.com/getfederated/prettyld"
+)
+
+type Person struct {
+	ID   string                     `json:"@id"`
+	Name prettyld.ValueNode[string] `json:"https://example.com/ns#name"`
+}
 
 func TestParseAndUnmarshal(t *testing.T) {
 	b := []byte(`{
@@ -13,18 +23,18 @@ func TestParseAndUnmarshal(t *testing.T) {
 	}`)
 
 	type Person struct {
-		ID   string            `json:"@id"`
-		Name ValueNode[string] `json:"https://example.com/ns#name"`
+		ID   string                     `json:"@id"`
+		Name prettyld.ValueNode[string] `json:"https://example.com/ns#name"`
 	}
 
-	p, err := Parse(b, nil)
+	p, err := prettyld.Parse(b, nil)
 	if err != nil {
 		t.Error(err)
 		t.FailNow()
 	}
 
 	var person Person
-	err = p.Unmarshal(&person)
+	err = p.UnmarshalTo(&person)
 	if err != nil {
 		t.Error(err)
 		t.FailNow()
@@ -33,5 +43,135 @@ func TestParseAndUnmarshal(t *testing.T) {
 	expected := "https://example.com"
 	if string(person.ID) != expected {
 		t.Errorf("expected %s but got %s", expected, string(person.ID))
+	}
+}
+
+func TestItems(t *testing.T) {
+	b := []byte(`[
+		{
+			"@context": {
+				"ex": "https://example.com/ns#",
+				"name": "ex:name"
+			},
+			"@id": "https://example.com/1",
+			"name": "Alice"
+		},
+		{
+			"@context": {
+				"ex": "https://example.com/ns#",
+				"name": "ex:name"
+			},
+			"@id": "https://example.com/2",
+			"name": "Bob"
+		}
+	]`)
+
+	p, err := prettyld.Parse(b, nil)
+	if err != nil {
+		t.Error(err)
+		t.FailNow()
+	}
+
+	expected := []Person{{
+		ID:   "https://example.com/1",
+		Name: prettyld.ValueNode[string]{Value: "Alice"},
+	}, {
+		ID:   "https://example.com/2",
+		Name: prettyld.ValueNode[string]{Value: "Bob"},
+	}}
+
+	actual := []Person{}
+
+	for item := range p.Items() {
+		var person Person
+
+		err := item.UnmarshalTo(&person)
+		if err != nil {
+			t.Error(err)
+			t.FailNow()
+		}
+
+		actual = append(actual, person)
+	}
+
+	slices.SortFunc(actual, func(i, j Person) int {
+		if i.ID < j.ID {
+			return -1
+		} else if i.ID > j.ID {
+			return 1
+		}
+		return 0
+	})
+
+	for i, person := range actual {
+		if person.ID != expected[i].ID {
+			t.Errorf("expected %s but got %s", expected[i].ID, person.ID)
+		}
+		if person.Name.Value != expected[i].Name.Value {
+			t.Errorf("expected %s but got %s", expected[i].Name.Value, person.Name.Value)
+		}
+	}
+}
+
+func TestNesting(t *testing.T) {
+	b := []byte(`{
+		"@context": {
+			"ex": "https://example.com/ns#",
+			"name": "ex:name",
+			"friend": {
+				"@id": "ex:friend",
+				"@type": "@id"
+			}
+		},
+		"@id": "https://example.com/1",
+		"name": "Alice",
+		"friend": {
+			"@id": "https://example.com/2",
+			"name": "Bob"
+		}
+	}`)
+
+	type Person struct {
+		ID     string                     `json:"@id"`
+		Name   prettyld.ValueNode[string] `json:"https://example.com/ns#name"`
+		Friend prettyld.LDNodes           `json:"https://example.com/ns#friend"`
+	}
+
+	p, err := prettyld.Parse(b, nil)
+	if err != nil {
+		t.Error(err)
+		t.FailNow()
+	}
+
+	var person Person
+	err = p.UnmarshalTo(&person)
+	if err != nil {
+		t.Error(err)
+		t.FailNow()
+	}
+
+	expected := "https://example.com/1"
+	if person.ID != expected {
+		t.Errorf("expected %s but got %s", expected, person.ID)
+	}
+
+	expected = "Alice"
+	if person.Name.Value != expected {
+		t.Errorf("expected %s but got %s", expected, person.Name.Value)
+	}
+
+	expected = "https://example.com/2"
+	for item := range person.Friend.Items() {
+		var friend Person
+
+		err := item.UnmarshalTo(&friend)
+		if err != nil {
+			t.Error(err)
+			t.FailNow()
+		}
+
+		if friend.ID != expected {
+			t.Errorf("expected %s but got %s", expected, friend.ID)
+		}
 	}
 }
