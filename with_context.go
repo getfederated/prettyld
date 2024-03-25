0a1d356620d9eb42d05e75c26aa66a540274b01e
Sal Rahman <salehen.rahman@gmail.com>
2024-03-25 16:06:17 -0700

    withContext -> ContextInstance

--- with_context.go
+++ with_context.go
@@ -2,14 +2,14 @@ package prettyld
 
 import "github.com/piprate/json-gold/ld"
 
-type withContext struct {
+type ContextInstance struct {
 	context any
 }
 
-func WithContext(context any) withContext {
-	return withContext{context}
+func WithContext(context any) ContextInstance {
+	return ContextInstance{context}
 }
 
-func (w withContext) MarshalCompactJSONLD(source any, options *ld.JsonLdOptions) ([]byte, error) {
+func (w ContextInstance) MarshalCompactJSONLD(source any, options *ld.JsonLdOptions) ([]byte, error) {
 	return marshalCompactJSONLD(source, w.context, options)
 }
